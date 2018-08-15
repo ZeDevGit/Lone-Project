5dd6873e44229833df0f98e7357895a61d8d8024
Edward Lawrence <elawrence.bda@gmail.com>
2018-08-14 21:36:13 -0300

    Player Movement
    Able to move and jump

--- Lone Game/Assets/Scripts/PlayerController.cs
+++ Lone Game/Assets/Scripts/PlayerController.cs
@@ -2,25 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public float speed;
-    private Rigidbody rb;
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    public int Health;
 
-
-
-	// Use this for initialization
-	void Start ()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        controller = GetComponent<CharacterController>();
+        Health = 3;
+    }
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
-	}
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+
+            {
+                moveDirection.y = jumpSpeed;
+            }
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "HealthPickUp")
+        {
+            if (Health < 3)
+            {
+                Health ++;
+                Destroy(col.gameObject);
+            }
+        }
+    }
 }

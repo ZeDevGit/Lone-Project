f940546f7d9b785e1abefa0c15f5a1967f7ac849
Edward Lawrence <elawrence.bda@gmail.com>
2018-08-24 19:57:40 -0300

    Double Jump and Orbiting Camera
    Able to move and double jump. Camera is now mouse controlled by player.

--- Lone Game/Assets/Scripts/PlayerController.cs
+++ Lone Game/Assets/Scripts/PlayerController.cs
@@ -5,34 +5,78 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
-    private CharacterController controller;
+    private const float FALL_MULT = 1;
+    private const float LOW_JUMP_MULT = 1.5f;
+
+    public float gravity;
+    public float Speed;
+    public float jumpSpeed;
+
+    public float moveX, moveZ;
+
+    public bool isGrounded;
+
+    public LayerMask GroundLayer;
+
+    private Rigidbody rb;
     public int Health;
+
+    public float VerticalVelocity;
+    public bool canDoubleJump;
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
         Health = 3;
     }
-    void Update()
+    void FixedUpdate()
     {
-        if (controller.isGrounded)
+        isGrounded = (Physics.Raycast(new Vector3(transform.position.x, transform.position.y - .5f, transform.position.z), Vector2.down, 0.6f, GroundLayer));
+
+        CheckInput();
+        Move();
+
+        adjustGravity();
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector3 (moveX * Speed, rb.velocity.y, moveZ * Speed);
+        // gameObject.transform.Translate (new Vector3(moveX * Speed, rb.velocity.y, moveZ * Speed));
+    }
+
+    void Jump()
+    {
+        // controller.AddForce(Vector3.up * jumpSpeed);
+        rb.AddForce (Vector3.up * jumpSpeed);
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetKeyDown(KeyCode.Space))
-
-            {
-                moveDirection.y = jumpSpeed;
-            }
-
+            Jump();
+            canDoubleJump = true;
         }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        else if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump)
+        {
+            Jump();
+            canDoubleJump = false;
+        }
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+    }
+
+    void adjustGravity ()
+    {
+        if (VerticalVelocity < 0 && isGrounded == false)
+        {
+            VerticalVelocity += Physics.gravity.y * FALL_MULT * Time.deltaTime;
+        }
+        if (VerticalVelocity > 0 && (!Input.GetKeyDown(KeyCode.Space)) && isGrounded == false)
+        {
+            VerticalVelocity += Physics.gravity.y * FALL_MULT * Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter(Collider col)
